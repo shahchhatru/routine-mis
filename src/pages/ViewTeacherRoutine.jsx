f6ff1e9d6a804cc397d7a7a846136b27d7ce4d0b
shahchhatru <shahkhesavchhatra@gmail.com>
2023-12-23 17:49:42 +0545

    logout for teachers

--- src/pages/ViewTeacherRoutine.jsx
+++ src/pages/ViewTeacherRoutine.jsx
@@ -7,15 +7,16 @@ import Tab from "@mui/material/Tab";
 import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
-import { redirect, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import TTeacherRoutine from "./TTeacherRoutine";
 import ViewRoutine from "./ViewRoutine";
 import AuthContext from "../context/authContext";
+import { Grid ,Button} from "@mui/material";
 
 export default function ViewTeacherRoutine() {
   const navigate=useNavigate();
   const [teacher,setTeacher]=React.useState();
-  const {user}=useContext(AuthContext);
+  const {user,logoutUser}=useContext(AuthContext);
   const {id}=useParams();
   const [iId,setId]=React.useState();
   React.useEffect(()=>{
@@ -45,7 +46,21 @@ export default function ViewTeacherRoutine() {
 
   return (
     iId?
-    <Box sx={{ width: "100%", typography: "body1" }}>
+    
+    <Box sx={{ width: "100%",height:'100%', typography: "body1" }}>
+      {
+        id!=iId?(
+          <Grid container style={{width:'100vw',display:'flex',paddding:4}} spacing={4}>
+          <Grid item sx={12}>
+            <Button onClick={()=>navigate('/changepassword')}>Change password</Button>
+          </Grid>
+          <Grid item sx={12} >
+            <Button variant="contained" onClick={()=>logoutUser()}>Logout</Button>
+          </Grid>
+        </Grid>
+        ):<></>
+      }
+     
       <TabContext value={value}>
         <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
           <TabList onChange={handleChange} aria-label="lab API tabs example">
